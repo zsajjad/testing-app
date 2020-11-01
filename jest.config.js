d0640f67e7298d046a8a841851d478d7c7b1f5f1
Zain Sajjad <zsajjad@fetchsky.com>
2020-11-02 03:48:08 +0500

    Added jest reportor

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,14 @@ module.exports = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   setupFiles: ['./jest.mock.js'],
+  reporters: [
+    'default',
+    [
+      'jest-html-reporters',
+      {
+        filename: 'jest.report.html',
+        expand: true,
+      },
+    ],
+  ],
 };
